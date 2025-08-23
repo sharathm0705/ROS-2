603187be3018757d70daa01921565c5aaa58065b
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-23 22:11:34 +0530

    Initial commit with only src

---
+++ src/my_py_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sharath',
+    maintainer_email='sharath@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "py_node = my_pkg.my_first_node:main    "
+        ],
+    },
+)
