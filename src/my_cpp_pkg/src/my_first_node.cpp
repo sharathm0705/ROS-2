d586c0ce1d9dede7a3e6840af2cc7999aada0ac4
sharathm0705 <sharathmnayak2005@gmail.com>
2025-09-07 21:30:04 +0530

    ros2 cpp

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,33 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
 
-class MyNode : public rclcpp :: Node
-{
-public:
-MyNode() : Node("Cpp_Test"),counter_(0){
-    RCLCPP_INFO(this->get_logger(),"Hello World !!!!");
-    timer_=this->create_wall_timer(std::chrono::seconds(1),
-                             std::bind(&MyNode::timerCallBack,this));
-}
-private:
-void timerCallBack ()
-{
-    // To send message continuosly :
-    RCLCPP_INFO(this->get_logger(),"Hello %d",counter_);
-    counter_++;
-
-}
-rclcpp::TimerBase::SharedPtr timer_;
-int counter_;
-
-};
-
-
-
-
-
-int main(int argc ,char **argv){
+int main(int argc,char **argv){
     rclcpp::init(argc,argv);
-    auto node = std::make_shared<MyNode>();
+    auto node=std::make_shared<rclcpp::Node>("cpp_test");
+    RCLCPP_INFO(node->get_logger(),"Hello World !!!");
     rclcpp::spin(node);
     rclcpp::shutdown();
+    return 0;
 }
